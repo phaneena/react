c060ab105368feac42253d2e37e28bc63ee4983b
phaneena <85494776+phaneena@users.noreply.github.com>
2024-09-12 17:06:37 +0530

    first commit

---
+++ src/components/Greet.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+// function Greet(){
+//     return <h1>hii hanneenaa</h1>
+// }
+
+const Greet=()=> <h1>hlo haneenaa</h1>
+
+export default Greet
---
+++ src/components/hello.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Hello=(props)=>{
+    // return  React.createElement
+    return (
+    <div>
+        <h1>hello hyyy {props.data}</h1>
+    </div>
+    )
+
+    // 'div',
+    // React.createElement('h1' ,null,'haneenaa')
+    // )
+}
+
+export default Hello
